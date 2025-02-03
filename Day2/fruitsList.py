4634ff2d12a110616cd6a3cd75ae01fac291f347
bibashmanjusubedi <acharabibash@outlook.com>
2025-02-03 15:41:51 +0545

    Lists modified

--- Day2/fruitsList.py
+++ Day2/fruitsList.py
@@ -1,51 +1,18 @@
-# Creating a list
-fruits = ["apple", "banana", "cherry", "date"]
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist)
+for item in thislist:
+    print(item)
 
-# Printing the entire list
-print("List of fruits:", fruits)
+# Acessing List Items
 
-# Accessing elements in a list by index
-print("First fruit:", fruits[0])  # Output: apple
-print("Second fruit:", fruits[1])  # Output: banana
+# list indexing
+print(thislist[0]) #1st item
+print(thislist[1]) # 2nd item in list
+print(thislist[-1]) # last item in list
 
-# Negative indexing (accessing elements from the end)
-print("Last fruit:", fruits[-1])  # Output: date
-print("Second last fruit:", fruits[-2])  # Output: cherry
-
-# Slicing a list (getting a sublist)
-print("First two fruits:", fruits[0:2])  # Output: ['apple', 'banana']
-print("Fruits from index 1 to 3:", fruits[1:4])  # Output: ['banana', 'cherry', 'date']
-
-# Modifying an element in the list
-fruits[1] = "blueberry"
-print("Updated list of fruits:", fruits)  # Output: ['apple', 'blueberry', 'cherry', 'date']
-
-# Adding an element to the end of the list
-fruits.append("elderberry")
-print("List after appending:", fruits)  # Output: ['apple', 'blueberry', 'cherry', 'date', 'elderberry']
-
-# Removing an element from the list
-fruits.remove("cherry")
-print("List after removing 'cherry':", fruits)  # Output: ['apple', 'blueberry', 'date', 'elderberry']
-
-# Checking if an element exists in the list
-if "banana" in fruits:
-    print("Banana is in the list")
-else:
-    print("Banana is not in the list")  # Output: Banana is not in the list
-
-# Length of the list
-print("Number of fruits in the list:", len(fruits))  # Output: 4
-
-# Looping through the list
-print("Fruits in the list:")
-for fruit in fruits:
-    print(fruit)
-
-# List comprehension (creating a new list based on existing list)
-uppercase_fruits = [fruit.upper() for fruit in fruits]
-print("Uppercase fruits:", uppercase_fruits)  # Output: ['APPLE', 'BLUEBERRY', 'DATE', 'ELDERBERRY']
-
-# Clearing the list
-fruits.clear()
-print("List after clearing:", fruits)  # Output: []
+# list slicing
+print(thislist[2:5]) # 3rd to 5th item
+print(thislist[:4]) # beginning to 3rd item
+print(thislist[2:]) # 3rd to last item
+print(thislist[1:5:2]) # 2nd to 5th item step size 2
+print(thislist[::2]) # 1st to last item step size 2
