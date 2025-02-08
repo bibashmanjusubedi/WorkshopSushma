5c48b96727b7993ddd622bad10fa02ff5a790720
bibashmanjusubedi <acharabibash@outlook.com>
2025-02-08 16:33:38 +0545

    Day2 little

--- Day2/cvbasics.py
+++ Day2/cvbasics.py
@@ -7,23 +7,31 @@ import cv2
 path = r'rajeshdai.jpg'
 
 # Reading an image in default mode
-image = cv2.imread(path)
+imageColor = cv2.imread(path)
 
 # Window name in which image is displayed
 window_name = 'rajeshsir'
 
 # Using cv2.imshow() method
 # Displaying the image
-cv2.imshow(window_name, image)
+cv2.imshow(window_name, imageColor)
 
 # waits for user to press any key
 # (this is necessary to avoid Python kernel form crashing)
 cv2.waitKey(0)
-
-image = cv2.imread(path,0) #grayscale read
-# print(image)
-cv2.imshow("b&w",image)
+print("Rajesh Dai in color")
+print(imageColor)
 cv2.waitKey()
+
+
+imageBW = cv2.imread(path,0) #grayscale read
+# print(image)
+cv2.imshow("b&w",imageBW)
+cv2.waitKey()
+print("Rajesh Dai in BW")
+print(imageBW)
+cv2.waitKey()
+
 
 
 image2 = cv2.imread(r'ORL(old)\p1\8.jpg',0)
