2155a372b5f53242dbfeea40ae5e69c43b0e6ce9
bibashmanjusubedi <acharabibash@outlook.com>
2025-02-09 19:45:19 +0545

    Constructor Class hierachy at the top of PCA class

--- Day3/PCA.py
+++ Day3/PCA.py
@@ -5,6 +5,16 @@ import scipy.linalg as s_linalg
 
 class pca_class:
 
+    def __init__(self, images, y, target_names, no_of_elements, quality_percent):
+        self.no_of_elements = no_of_elements
+        self.images = np.asarray(images)
+        self.y = y
+        self.target_names = target_names
+        mean = np.mean(self.images, 1)
+        self.mean_face = np.asmatrix(mean).T
+        self.images = self.images - self.mean_face
+        self.quality_percent = quality_percent
+
 
     def give_p(self, d):
         sum = np.sum(d)
@@ -28,17 +38,6 @@ class pca_class:
         print("new bases",self.new_bases.shape)
         self.new_coordinates = np.dot(self.new_bases.T, self.images)
         return self.new_coordinates.T
-
-
-    def __init__(self, images, y, target_names, no_of_elements, quality_percent):
-        self.no_of_elements = no_of_elements
-        self.images = np.asarray(images)
-        self.y = y
-        self.target_names = target_names
-        mean = np.mean(self.images, 1)
-        self.mean_face = np.asmatrix(mean).T
-        self.images = self.images - self.mean_face
-        self.quality_percent = quality_percent
 
     def original_data(self, new_coordinates):
         return self.mean_face + (np.dot(self.new_bases, new_coordinates.T))
